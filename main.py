665dc3c555202e64ccaa4b7e91e276ae2bfdf0b1
Samuel Cochran <sj26@sj26.com>
2024-11-13 13:00:23 +1100

    Add and use a test dependency

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from python_test_dependency import buildkite
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return f"<p>Hello, World!</p>"
+    return f"<p>Hello, {buildkite()}!</p>"
 
 if __name__ == "__main__":
     app.run()
